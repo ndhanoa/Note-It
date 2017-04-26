82606604eabebeac3bd8afe19d2d47565a6e5c89
ndhanoa <ndhanoa@wireless-s1-no-150-25-224.uchicago.edu>
2017-04-26 10:02:51 -0500

    Updated Controller

--- NetBeansProjects/NoteIt/src/noteit/CompositionController.java
+++ NetBeansProjects/NoteIt/src/noteit/CompositionController.java
@@ -5,6 +5,7 @@
  */
 package noteit;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -12,9 +13,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Line;
+import javafx.stage.FileChooser;
 
 public class CompositionController implements Initializable {
     
@@ -48,11 +51,6 @@ public class CompositionController implements Initializable {
     @FXML 
     private ImageView quarterNoteForStaff;
 
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
     ImageView details;
     @FXML
     private void handleClickQuarterNote(MouseEvent me) {
@@ -60,7 +58,18 @@ public class CompositionController implements Initializable {
         save.setText("clicked!");
         quarterNoteForStaff.setVisible(true);
     }
-    
+     @FXML
+    private ListView fileList;
+    private FileChooser fileChooser;
+    private void handleButtonAction(ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        File selectedFile = fc.showOpenDialog(null);
+        if(selectedFile != null){
+            fileList.getItems().add(selectedFile.getName());
+        } else {
+            System.out.println("Error: File is not valid.");
+        }
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
