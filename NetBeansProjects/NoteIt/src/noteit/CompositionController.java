5002cefc450257490f901743c78a4f4b2bc1241c
Alice <Alice@wireless-s0-so-150-32-76.uchicago.edu>
2017-04-25 19:32:33 -0500

    Merge branch 'master' of https://github.com/ndhanoa/Note-It
    Conflicts:
    	NetBeansProjects/NoteIt/src/noteit/CompositionController.java
    	NetBeansProjects/NoteIt/src/noteit/FXMLDocument.fxml

--- NetBeansProjects/NoteIt/src/noteit/CompositionController.java
+++ NetBeansProjects/NoteIt/src/noteit/CompositionController.java
@@ -13,7 +13,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Line;
+import javax.media.j3d.View;
+import static javax.media.j3d.View.VISIBILITY_DRAW_VISIBLE;
 
 public class CompositionController implements Initializable {
     
@@ -43,16 +46,27 @@ public class CompositionController implements Initializable {
     
     @FXML
     private ImageView quarternoteImage;
+    
+    @FXML 
+    private ImageView quarterNoteForStaff;
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
+    ImageView details;
+    @FXML
+    private void handleClickQuarterNote(MouseEvent me) {
+        
+        save.setText("clicked!");
+        quarterNoteForStaff.setVisible(true);
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        quarterNoteForStaff.setVisible(false);
     }    
     
 }
